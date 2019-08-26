aef0c28533191782ab3fd582a793aecf9e595326
Alexander Barthel <albar965@mailbox.org>
2019-08-26 20:32:30 +0200

    Version set to 1.0.13

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
   QCoreApplication::setOrganizationName("ABarthel");
   QCoreApplication::setOrganizationDomain("littlenavmap.org");
 
-  QCoreApplication::setApplicationVersion("1.0.12"); // VERSION_NUMBER
+  QCoreApplication::setApplicationVersion("1.0.13"); // VERSION_NUMBER
 
   try
   {
