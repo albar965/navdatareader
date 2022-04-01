d344d7111f75eb86b27a3eb188abe87d1a53b2c8
Alexander Barthel <albar965@mailbox.org>
2022-04-01 13:30:09 +0200

    Adapted to changes in atools regarding to XP12 support. albar965/littlenavmap#867

--- navdatareader.cpp
+++ navdatareader.cpp
@@ -136,7 +136,7 @@ void NavdataReader::parseArgs()
 
   QCommandLineOption fstypeOpt({"f", "flight-simulator"},
                                QObject::tr("Required option. Flight simulator type <simulator> or other data source. "
-                                           "Either FSX, FSXSE, P3DV2, P3DV3, P3DV4, P3DV5, XP11, MSFS or DFD."),
+                                           "Either FSX, FSXSE, P3DV2, P3DV3, P3DV4, P3DV5, XP11, XP12, MSFS or DFD."),
                                QObject::tr("simulator"));
   parser.addOption(fstypeOpt);
 
@@ -188,7 +188,7 @@ void NavdataReader::parseArgs()
     opts.setSimulatorType(type);
   }
 
-  if(type == FsPaths::UNKNOWN)
+  if(type == FsPaths::NONE)
   {
     qCritical().noquote().nospace() << "*** ERROR: Unknown type for option -f." << endl;
     parser.showHelp(1);
@@ -201,7 +201,7 @@ void NavdataReader::parseArgs()
     parser.showHelp(1);
   }
 
-  if(type == FsPaths::XPLANE11 && !parser.isSet(basepathOpt))
+  if(FsPaths::isAnyXplane(type) && !parser.isSet(basepathOpt))
   {
     qCritical().noquote().nospace() << "*** ERROR: No base path for X-Plane given." << endl;
     parser.showHelp(1);
@@ -236,7 +236,7 @@ void NavdataReader::parseArgs()
   }
 
   // Scenery.cfg only FSX and P3D ===================================================
-  if(type != FsPaths::XPLANE11 && type != FsPaths::DFD && type != FsPaths::MSFS)
+  if(!FsPaths::isAnyXplane(type) && type != FsPaths::DFD && type != FsPaths::MSFS)
   {
     QString sceneryFile = parser.value(sceneryOpt);
     if(sceneryFile.isEmpty())
