f8d8c0d065e9b4bad02f161f6eb4a45b9ddc5e38
Alexander Barthel <albar965@mailbox.org>
2024-09-28 16:25:53 +0200

    Corrected deployment name for Linux builds.

--- build/linux/pack_deploy.sh
+++ build/linux/pack_deploy.sh
@@ -4,8 +4,6 @@
 set -e
 set -x
 
-export TARGET_NAME=Navdatareader
-
 # Error checking for required variable APROJECTS
 if [ -z "$APROJECTS" ] ; then echo APROJECTS environment variable not set ; exit 1 ; fi
 if [ ! -d "$APROJECTS" ]; then echo "$APROJECTS" does not exist ; exit 1 ; fi
@@ -13,20 +11,25 @@ if [ ! -d "$APROJECTS" ]; then echo "$APROJECTS" does not exist ; exit 1 ; fi
 # Override by envrionment variable for another target
 export SSH_DEPLOY_TARGET=${SSH_DEPLOY_TARGET:-"sol:/data/alex/Public/Releases"}
 
+# Navdatareader-linux-ubuntu-22.04-2.8.2.beta.tar.xz
 if [ -f "/etc/os-release" ]; then
   source /etc/os-release
-  export FILENAME=$DISTRIB_RELEASE-$(head -n1 ${APROJECTS}/deploy/"$TARGET_NAME"/version.txt)
+  export FILENAME_LNM=$ID-$VERSION_ID-$(head -n1 "${APROJECTS}/deploy/Navdatareader/version.txt")
 else
-  export FILENAME=$(head -n1 ${APROJECTS}/deploy/"$TARGET_NAME"/version.txt)
+  export FILENAME_LNM=$(head -n1 "${APROJECTS}/deploy/Navdatareader/version.txt")
 fi
-
-export FILENAME=$DISTRIB_RELEASE-$(head -n1 ${APROJECTS}/deploy/"$TARGET_NAME"/version.txt)
 
 (
   cd ${APROJECTS}/deploy
-  tar cfvz "$TARGET_NAME.tar.gz" "$TARGET_NAME"
+
+  rm -rfv "Navdatareader-linux-${FILENAME_LNM}"
+  rm -fv Navdatareader-linux-${FILENAME_LNM}.tar.gz Navdatareader-linux-${FILENAME_LNM}.tar.xz
+
+  cp -av "Navdatareader" "Navdatareader-linux-${FILENAME_LNM}"
+
+  tar cv "Navdatareader-linux-${FILENAME_LNM}" | xz -9 > Navdatareader-linux-${FILENAME_LNM}.tar.xz
 )
 
-scp "${APROJECTS}/deploy/$TARGET_NAME.tar.gz" "${SSH_DEPLOY_TARGET}/$TARGET_NAME-linux-${FILENAME}.tar.gz"
+scp ${APROJECTS}/deploy/Navdatareader-linux-${FILENAME_LNM}.tar.xz ${SSH_DEPLOY_TARGET}/
 
 
