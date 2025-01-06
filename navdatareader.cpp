1e896f2cd16493fe12dd95046893f2f1d1c4c9b4
Alexander Barthel <albar965@mailbox.org>
2025-01-06 22:44:32 +0100

    Removed streamed packages path for MFSS 2024
    Content was pulled during an update. Now only option is SimConnect.
    albar965/littlenavmap#1198

--- navdatareader.cpp
+++ navdatareader.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2025 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -237,8 +237,6 @@ void NavdataReader::parseArgs()
     opts.setMsfsCommunityPath(FsPaths::getMsfsCommunityPath(opts.getBasepath()));
     opts.setMsfsOfficialPath(FsPaths::getMsfsOfficialPath(opts.getBasepath()));
   }
-  else if(type == FsPaths::MSFS_2024)
-    opts.setMsfs24StreamedPackagesPath(FsPaths::getMsfs24StreamedPackagesPath());
 
   // Configuration file ===================================================
   configFile = parser.value(cfgOpt);
